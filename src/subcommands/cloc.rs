2e2e8ccabec033eaaadd3dae8b5ba2c5b0f3243b
Volker Kempert <volker.kempert@almedso.de>
2024-10-04 20:14:18 +0200

    cloc: Add initial capabilities via tokei crate

--- src/subcommands/cloc.rs
+++ src/subcommands/cloc.rs
@@ -3,7 +3,7 @@ use log::info;
 
 pub const COMMAND: &str = "cloc";
 
-// use tokei;
+use tokei::{Config, Languages, LanguageType};
 
 #[macro_export]
 macro_rules! cloc_command {
@@ -17,6 +17,16 @@ macro_rules! cloc_command {
     };
 }
 
-pub fn run(_common_args: CommonArgs) {
+pub fn run(common_args: CommonArgs) {
     info!("Run cloc - count lines of code");
+
+    let config = Config::default();
+    let mut languages = Languages::new();
+    let paths = &[ common_args.project_dir ];
+    let excluded = &[ "target", "build"];
+
+    languages.get_statistics(paths, excluded, &config);
+    let rust = &languages[&LanguageType::Rust];
+
+    println!("Lines of code: {}", rust.code);
 }
